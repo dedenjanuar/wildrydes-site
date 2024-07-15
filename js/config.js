267cc5ea04f1be425796ec6d27ca95e2f439f981
Deden <deden.januar@icube-mobile.com>
2024-07-15 10:36:02 +0700

    new config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_diwAED1nX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '36f8323rgtd5m6fl9gno5gppc2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_EBp6CWGp7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3fq7u0jvb3b7hno45mek2sjk7p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://741g024jac.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
